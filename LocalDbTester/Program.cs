89cb21817787a6c96aa6a71008344f73cc74b340
Dimitrie Stefanescu <didimitrie@gmail.com>
2018-10-29 17:47:09 +0000

    mostly project config for local dbs

---
+++ LocalDbTester/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SpeckleCore;
+
+namespace LocalDbTester
+{
+  class Program
+  {
+    static void Main( string[ ] args )
+    {
+      Console.WriteLine( "Hello World!" );
+      SpeckleLocalContext.Init();
+
+      var x = SpeckleLocalContext.GetAccountByRestApi( "asdfa" );
+
+      Console.WriteLine( x.Count );
+
+      Console.WriteLine( "Press enter to close..." );
+      Console.ReadLine();
+    }
+  }
+}
