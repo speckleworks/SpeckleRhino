234e301d01a4ec42918bdf4d9b38d5393b12f870
spsarras <psarras.st@gmail.com>
2020-01-17 15:07:28 +0000

    added GenericAccess parameter to the CreateSpeckleObject. This now allows for arbitary structures to be created.

--- SpeckleGrasshopper/ObjectCreation/CreateSpeckleObject.cs
+++ SpeckleGrasshopper/ObjectCreation/CreateSpeckleObject.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
 using Grasshopper.Kernel.Parameters;
+using Grasshopper.Kernel.Types;
 using SpeckleCore;
 
 namespace SpeckleGrasshopper
@@ -16,35 +18,35 @@ namespace SpeckleGrasshopper
   {
     private Timer Debouncer;
 
-    public CreateSpeckleObject( )
-      : base( "Create a Custom Speckle Object", "CSO",
+    public CreateSpeckleObject()
+      : base("Create a Custom Speckle Object", "CSO",
           "Creates a custom speckle object with whatever propeties you want. You can set them at your ease.",
-          "Speckle", "Special" )
+          "Speckle", "Special")
     {
       this.Params.ParameterNickNameChanged += Params_ParameterNickNameChanged;
     }
 
-    public override void AddedToDocument( GH_Document document )
+    public override void AddedToDocument(GH_Document document)
     {
-      base.AddedToDocument( document );
-      Debouncer = new Timer( 2000 ); Debouncer.AutoReset = false;
-      Debouncer.Elapsed += ( sender, e ) =>
+      base.AddedToDocument(document);
+      Debouncer = new Timer(2000); Debouncer.AutoReset = false;
+      Debouncer.Elapsed += (sender, e) =>
       {
-        Rhino.RhinoApp.MainApplicationWindow.Invoke( ( Action ) delegate { this.ExpireSolution( true ); } );
+        Rhino.RhinoApp.MainApplicationWindow.Invoke((Action)delegate { this.ExpireSolution(true); });
       };
 
-      foreach ( var param in Params.Input )
+      foreach (var param in Params.Input)
       {
-        param.ObjectChanged += ( sender, e ) =>
+        param.ObjectChanged += (sender, e) =>
         {
           Debouncer.Start();
         };
       }
     }
 
-    private void Params_ParameterNickNameChanged( object sender, GH_ParamServerEventArgs e )
+    private void Params_ParameterNickNameChanged(object sender, GH_ParamServerEventArgs e)
     {
-      ExpireSolution( true );
+      ExpireSolution(true);
     }
 
     /// <summary>
@@ -52,7 +54,7 @@ namespace SpeckleGrasshopper
     /// </summary>
     public override Guid ComponentGuid
     {
-      get { return new Guid( "{7b758641-d781-4e15-bfd8-b6c723c9dd28}" ); }
+      get { return new Guid("{7b758641-d781-4e15-bfd8-b6c723c9dd28}"); }
     }
 
     /// <summary>
@@ -66,9 +68,9 @@ namespace SpeckleGrasshopper
       }
     }
 
-    public bool CanInsertParameter( GH_ParameterSide side, int index )
+    public bool CanInsertParameter(GH_ParameterSide side, int index)
     {
-      if ( side == GH_ParameterSide.Input )
+      if (side == GH_ParameterSide.Input)
       {
         return true;
       }
@@ -78,9 +80,9 @@ namespace SpeckleGrasshopper
       }
     }
 
-    public bool CanRemoveParameter( GH_ParameterSide side, int index )
+    public bool CanRemoveParameter(GH_ParameterSide side, int index)
     {
-      if ( side == GH_ParameterSide.Input && Params.Input.Count > 1 )
+      if (side == GH_ParameterSide.Input && Params.Input.Count > 1)
       {
         return true;
       }
@@ -90,92 +92,140 @@ namespace SpeckleGrasshopper
       }
     }
 
-    public IGH_Param CreateParameter( GH_ParameterSide side, int index )
+    public IGH_Param CreateParameter(GH_ParameterSide side, int index)
     {
-      Grasshopper.Kernel.Parameters.Param_GenericObject param = new Param_GenericObject();
+      var param = new Param_GenericAccess();
 
-      param.Name = GH_ComponentParamServer.InventUniqueNickname( "ABCDEFGHIJKLMNOPQRSTUVWXYZ", Params.Input );
+      param.Name = GH_ComponentParamServer.InventUniqueNickname("ABCDEFGHIJKLMNOPQRSTUVWXYZ", Params.Input);
       param.NickName = param.Name;
       param.Description = "Property Name";
       param.Optional = true;
       param.Access = GH_ParamAccess.item;
 
-      param.ObjectChanged += ( sender, e ) => Debouncer.Start();
+      param.ObjectChanged += (sender, e) => Debouncer.Start();
 
       return param;
     }
 
-    public bool DestroyParameter( GH_ParameterSide side, int index )
+    public bool DestroyParameter(GH_ParameterSide side, int index)
     {
       return true;
     }
 
-    public void VariableParameterMaintenance( )
+    public void VariableParameterMaintenance()
     {
       //ExpireSolution( true );
     }
 
-    protected override void RegisterInputParams( GH_InputParamManager pManager )
+    protected override void RegisterInputParams(GH_InputParamManager pManager)
     {
-      pManager.AddGenericParameter( "A", "A", "Data.", GH_ParamAccess.item );
+      pManager.AddParameter(new Param_GenericAccess(), "A", "A", "Data.", GH_ParamAccess.item);
     }
 
-    protected override void RegisterOutputParams( GH_OutputParamManager pManager )
+    protected override void RegisterOutputParams(GH_OutputParamManager pManager)
     {
-      pManager.AddGenericParameter( "Speckle Object", "SO", "The newly created speckle object.", GH_ParamAccess.item );
-      pManager.AddGenericParameter( "Dictionary", "D", "Just the dictionary.", GH_ParamAccess.item );
+      pManager.AddGenericParameter("Speckle Object", "SO", "The newly created speckle object.", GH_ParamAccess.item);
+      pManager.AddGenericParameter("Dictionary", "D", "Just the dictionary.", GH_ParamAccess.item);
     }
 
-    protected override void SolveInstance( IGH_DataAccess DA )
+    protected override void SolveInstance(IGH_DataAccess DA)
     {
 
       var check = ValidateKeys();
-      if ( check.Item1 )
+      if (check.Item1)
       {
-        AddRuntimeMessage( GH_RuntimeMessageLevel.Error, check.Item2 );
+        AddRuntimeMessage(GH_RuntimeMessageLevel.Error, check.Item2);
         return;
       }
 
       var myDictionary = new Dictionary<string, object>();
 
-      for ( int i = 0; i < Params.Input.Count; i++ )
+      for (int i = 0; i < Params.Input.Count; i++)
       {
-        var key = Params.Input[ i ].NickName;
+        var key = Params.Input[i].NickName;
 
-        object ghInputProperty = null;
-        DA.GetData( i, ref ghInputProperty );
+        dynamic ghInputProperty = null;
+        object valueExtract = null;
+        switch (Params.Input[i].Access)
+        {
+          case GH_ParamAccess.item:
+            DA.GetData(i, ref ghInputProperty);
+            valueExtract = ghInputProperty?.Value;
+            break;
+          case GH_ParamAccess.list:
+            var dataValues = new List<dynamic>();
+            DA.GetDataList(i, dataValues);
+            valueExtract = dataValues.Select(x => x.Value).ToList();
+            break;
+          case GH_ParamAccess.tree:
+            var tree = new GH_Structure<IGH_Goo>();
+            DA.GetDataTree(i, out tree);
+            var dict = new Dictionary<string, IEnumerable<object>>();
 
-        if ( ghInputProperty == null ) continue;
+            for (int j = 0; j < tree.PathCount; j++)
+            {
+              var branch = tree.Branches[j];
+              var list = new List<object>();
+              for (int k = 0; k < branch.Count; k++)
+              {
+                list.Add(branch[k].GetType().GetProperty("Value").GetValue(branch[k], null));
+              }
+              //var value = branch.Select(x => x.GetType().GetProperty("Value").GetValue(x, null));
+              dict.Add(j.ToString(), list);
+            }
 
-        var valueExtract = ghInputProperty.GetType().GetProperty( "Value" ).GetValue( ghInputProperty, null );
+            myDictionary.Add(key, dict);
+            continue;
+          default:
+            continue;
+        }
+
+
+        //if (ghInputProperty == null) continue;
+
+        //var valueExtract = ghInputProperty?.Value;//.GetType().GetProperty( "Value" ).GetValue( ghInputProperty, null );
 
         try
         {
           var tests = valueExtract is IEnumerable<object>;
-
-          if ( valueExtract is IEnumerable<int> || valueExtract is IEnumerable<double> || valueExtract is IEnumerable<string> || valueExtract is IEnumerable<bool> )
+          if (valueExtract is IEnumerable<object>)
           {
-            myDictionary.Add( key, valueExtract );
+            var val = valueExtract as List<object>;
+            //Is this important? It replaced IEnumerable<int> 
+            if(val.Count > 0 && val[0] is int || val[0] is double || val[0] is string || val[1] is bool)
+            {
+              myDictionary.Add(key, val);
+            }
+            else
+            {
+              myDictionary.Add(key, Converter.Serialise(val));
+            }
           }
-          else if ( valueExtract is IEnumerable<object> )
-          {
-            //valueExtract = ( ( IEnumerable<object> ) valueExtract ).Select( o => o.GetType().GetProperty( "Value" ).GetValue( o, null ) );
-            myDictionary.Add( key, Converter.Serialise( valueExtract as IEnumerable<object> ) );
-          }
 
-          else if ( valueExtract is System.Collections.IDictionary )
+
+          //if (valueExtract is IEnumerable<int> || valueExtract is IEnumerable<double> || valueExtract is IEnumerable<string> || valueExtract is IEnumerable<bool>)
+          //{
+          //  //myDictionary.Add(key, valueExtract);
+          //}
+          //else if (valueExtract is IEnumerable<object>)
+          //{
+          //  //valueExtract = ( ( IEnumerable<object> ) valueExtract ).Select( o => o.GetType().GetProperty( "Value" ).GetValue( o, null ) );
+          //  //myDictionary.Add(key, Converter.Serialise(valueExtract as IEnumerable<object>));
+          //}
+
+          else if (valueExtract is System.Collections.IDictionary)
           {
-            myDictionary.Add( key, valueExtract );
+            myDictionary.Add(key, valueExtract);
           }
           else
           {
-            if ( valueExtract is bool || valueExtract is string || valueExtract is double || valueExtract is int )
-              myDictionary.Add( key, valueExtract );
+            if (valueExtract is bool || valueExtract is string || valueExtract is double || valueExtract is int)
+              myDictionary.Add(key, valueExtract);
             else
-              myDictionary.Add( key, Converter.Serialise( valueExtract ) );
+              myDictionary.Add(key, Converter.Serialise(valueExtract));
           }
         }
-        catch ( Exception e )
+        catch (Exception)
         {
           continue;
         }
@@ -184,48 +234,48 @@ namespace SpeckleGrasshopper
       var myObject = new SpeckleObject() { Properties = myDictionary };
       myObject.GenerateHash();
 
-      DA.SetData( 0, myObject );
-      DA.SetData( 1, myDictionary );
+      DA.SetData(0, myObject);
+      DA.SetData(1, myDictionary);
       //new GH_SpeckleObject()
     }
 
-    public Tuple<bool, string> ValidateKeys( )
+    public Tuple<bool, string> ValidateKeys()
     {
       List<string> keyNames = new List<string>();
       bool hasErrors = false;
       string validationErrors = "";
-      for ( int i = 0; i < Params.Input.Count; i++ )
+      for (int i = 0; i < Params.Input.Count; i++)
       {
-        var param = Params.Input[ i ];
-        if ( keyNames.Contains( param.NickName ) )
+        var param = Params.Input[i];
+        if (keyNames.Contains(param.NickName))
         {
-          this.AddRuntimeMessage( GH_RuntimeMessageLevel.Error, "Duplicate  key names found (" + param.NickName + "). Please use different values." );
+          this.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Duplicate  key names found (" + param.NickName + "). Please use different values.");
 
           validationErrors += "Duplicate  key names found (" + param.NickName + "). Please use different values.\n";
 
           hasErrors = true;
         }
 
-        if ( param.NickName == "type" || param.NickName == "Type" )
+        if (param.NickName == "type" || param.NickName == "Type")
         {
-          this.AddRuntimeMessage( GH_RuntimeMessageLevel.Error, "Using 'Type' or 'type' as a key name is not possible. Please use different name, for example 'familiyType'. Thanks!" );
+          this.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Using 'Type' or 'type' as a key name is not possible. Please use different name, for example 'familiyType'. Thanks!");
 
           validationErrors += "Using 'Type' or 'type' as a key name is not possible. Please use different name, for example 'familiyType'. Thanks!";
 
           hasErrors = true;
         }
 
-        if ( param.NickName.Contains( "." ) )
+        if (param.NickName.Contains("."))
         {
-          this.AddRuntimeMessage( GH_RuntimeMessageLevel.Error, "Dots in key names are not supported. Sorry!" );
+          this.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Dots in key names are not supported. Sorry!");
 
           validationErrors += "Dots in key names are not supported. Sorry!";
         }
 
-        keyNames.Add( param.NickName );
+        keyNames.Add(param.NickName);
       }
 
-      return new Tuple<bool, string>( hasErrors, validationErrors );
+      return new Tuple<bool, string>(hasErrors, validationErrors);
     }
   }
 }
---
+++ SpeckleGrasshopper/Parameters/Param_GenericAccess.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+using GH_IO.Serialization;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
+
+public class Param_GenericAccess : Param_GenericObject, IDisposable
+{
+  private const string ParamAccessKey = "ParamAccess";
+  public override GH_Exposure Exposure => GH_Exposure.hidden;
+
+  public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
+  {
+    base.AppendAdditionalMenuItems(menu);
+    if (Kind != GH_ParamKind.output)
+    {
+      var item0 = Menu_AppendItem(menu, "Item", Menu_Clicked, true, Access == GH_ParamAccess.item);
+      item0.ToolTipText = "Make this parameter an Item";
+      var item1 = Menu_AppendItem(menu, "Array", Menu_Clicked, true, Access == GH_ParamAccess.list);
+      item1.ToolTipText = "Make this parameters an Array";
+      var item2 = Menu_AppendItem(menu, "Tree", Menu_Clicked, true, Access == GH_ParamAccess.tree);
+      item2.ToolTipText = "Make this parameter a Tree";
+    }
+  }
+
+  public void Dispose()
+  {
+    ClearData();
+  }
+
+  private void Menu_Clicked(object sender, EventArgs e)
+  {
+    var menu = sender as ToolStripMenuItem;
+    var name = menu.AccessibilityObject.Name;
+    RecordUndoEvent("Changing State");
+
+    if (name.Equals("Tree"))
+    {
+      //RecordUndoEvent("tree");
+      Access = GH_ParamAccess.tree;
+    }
+    else if (name.Equals("Array"))
+    {
+      //RecordUndoEvent("array");
+      Access = GH_ParamAccess.list;
+    }
+    else if (name.Equals("Item"))
+    {
+      //RecordUndoEvent("item");
+      Access = GH_ParamAccess.item;
+    }
+
+    OnObjectChanged(GH_ObjectEventType.DataMapping);
+    ExpireSolution(true);
+
+  }
+
+  public override bool Read(GH_IReader reader)
+  {
+    var result = base.Read(reader);
+    if (reader.ItemExists(ParamAccessKey))
+    {
+      try
+      {
+        //In case casting produces invalid Access enum
+        Access = (GH_ParamAccess)reader.GetInt32(ParamAccessKey);
+      }
+      catch (Exception)
+      {
+
+      }
+    }
+    return result;
+  }
+
+  public override Guid ComponentGuid => new Guid("{2E711E3A-573E-42AD-86FF-0B6BA23E9990}");
+
+
+  public override bool Write(GH_IWriter writer)
+  {
+    var result = base.Write(writer);
+    writer.SetInt32(ParamAccessKey, (int)Access);
+    return result;
+  }
+
+}
+
+
